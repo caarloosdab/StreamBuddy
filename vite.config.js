f7ec99e9a25fa237e953aa737b6269f3c667aaa1
caarloosdab <caarloosdab@gmail.com>
2025-08-06 20:55:48 -0600

    `Added rollupOptions to vite.config.js with input files for build process`

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/', // Use '/' for Netlify
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        watchlist: resolve(__dirname, 'watchlist.html'),
+        details: resolve(__dirname, 'details.html'),
+        spinner: resolve(__dirname, 'spinner.html')
+      }
+    }
+  }
 })
